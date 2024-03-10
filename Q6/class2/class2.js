772ef990d78f148fe94b87260ece59136bd53751
Coderkainat <159674299+Coderkainat@users.noreply.github.com>
2024-03-10 22:37:41 +0500

    Add files via upload

---
+++ Q6/class2/class2.js
@@ -0,0 +1,55 @@
+// let firstName ="Muhammad";
+// firstName = "Ali";
+// console.log(firstName)
+// ------const-----//
+/*const color = "pink";
+console.log(color)
+
+color ="red";
+// ------let update-----//
+
+let name1 = "Ali";
+let name2 ="Muhammad";
+
+let age = 26
+console.log(age)
+//----var----//
+
+var name2 ="kainat";
+var name2 ="kainat Bilal"
+console.log(name2)
+//-----let----//;
+
+let myName ="johny";
+console.log(myName);
+//-----const-----//
+
+const myName ="Hani";
+console.log(myName);
+//----var---//
+
+var myCar = "suzuki"
+var myCar = "suzuki";
+console.log(myCar);
+//-------let update-----//
+
+let myName ="herry";
+console.log(myName);
+
+myName = "Herry john";
+console.log(myName);
+//----number----//
+
+let age = 25
+console.log(age)
+
+var myCar ="Alto";
+var myCar ="fortuner";
+console.log(myCar)/*/
+var myName = "Faizan"; //string
+var age = " 27"; //number
+var isFollow = "true"; //boolean
+var value = "undefined"; //undefined
+var newValue = "null"; //null
+myName = "Ali";
+myName = 25;
---
+++ Q6/class2/class2.ts
@@ -0,0 +1,66 @@
+// let firstName ="Muhammad";
+
+
+// firstName = "Ali";
+// console.log(firstName)
+// ------const-----//
+
+/*const color = "pink";
+console.log(color)
+
+color ="red";
+// ------let update-----//
+
+let name1 = "Ali";
+let name2 ="Muhammad";
+
+let age = 26
+console.log(age)
+//----var----//
+
+var name2 ="kainat";
+var name2 ="kainat Bilal"
+console.log(name2)
+//-----let----//;
+
+let myName ="johny";
+console.log(myName);
+//-----const-----//
+
+const myName ="Hani";
+console.log(myName);
+//----var---//
+
+var myCar = "suzuki"
+var myCar = "suzuki";
+console.log(myCar);
+//-------let update-----//
+
+let myName ="herry";
+console.log(myName);
+
+myName = "Herry john";
+console.log(myName);
+//----number----//
+
+let age = 25
+console.log(age)
+
+var myCar ="Alto";
+var myCar ="fortuner";
+console.log(myCar)/*/
+
+
+let myName :string ="Faizan"; //string
+let age =" 27"; //number
+let isFollow ="true";  //boolean
+let value ="undefined"; //undefined
+let newValue = "null" //null
+myName = "Ali"
+myName = 25;
+
+
+
+
+
+
